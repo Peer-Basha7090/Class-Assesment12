94a64aaf0b67cf273e4edca7b8f33772f118fcb6
Basha <Basha@123@gmail.com>
2026-04-17 16:38:51 +0530

    Even Or Odd Program

---
+++ EvenORodd.java
@@ -0,0 +1,15 @@
+class main 
+{
+    public static void main(String args[])
+    {
+        int n=5;
+         if(n % 2 == 0)
+         {
+            System.out.println("Even");
+         }
+         else
+         {
+            System.out.println("odd");
+         }
+    }
+}
