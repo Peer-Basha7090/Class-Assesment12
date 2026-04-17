7096c7d7f4ebaf4893014007bd708955e28af5ea
Basha <Basha@123@gmail.com>
2026-04-17 20:12:56 +0530

    LeapYear Program

---
+++ leapyear.java
@@ -0,0 +1,15 @@
+class main 
+{
+    public static void main(String args[])
+    {
+       int year=2000;
+       if((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+       {
+        System.out.println("Leap year");
+       } 
+       else 
+       {
+        System.out.println("Not leap year");
+       }
+    }
+}
